99a077c5d25c7bdd1c0ad6e2045d8ff75c8ceb15
Fixel <zielnyfranek@gmail.com>
2023-01-26 21:10:43 +0100

    Added transitions

--- Blackjack with the devil/Assets/Scripts/SceneController.cs
+++ Blackjack with the devil/Assets/Scripts/SceneController.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private Animator transitionAnimator;
+
     public void LoadScene(string name)
     {
+        transitionAnimator.SetTrigger("show");
+        StartCoroutine(LoadSceneAfterTransition(name));
+    }
+
+    private IEnumerator LoadSceneAfterTransition(string name)
+    {  
+        yield return new WaitUntil(() => HasTransitionFinished());
         SceneManager.LoadScene(name);
+    }
+
+    private bool HasTransitionFinished()
+    {
+        
+        return transitionAnimator.GetCurrentAnimatorStateInfo(0).IsName("transition_show") && transitionAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f;
     }
 }
